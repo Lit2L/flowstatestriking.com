f2cee786096744fe4247d8abc208124e34d4c9df
LarbeartoW <larbeartow@gmail.com>
2026-02-02 23:18:32 -0800

    small fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	async redirects() {
-		return [
-			{ source: '/book', destination: '/assessment', permanent: true },
-			{ source: '/book/:path*', destination: '/assessment', permanent: true }
-		]
-	}
-}
+const nextConfig = {}
 
 module.exports = nextConfig
